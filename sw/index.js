8a943ff90d11722fda5e0cc854e3ffe4333da465
Ryan Wade <ryanwade1928@gmail.com>
2019-04-29 18:05:27 -0500

    Added start script

--- sw/index.js
+++ sw/index.js
@@ -1,21 +1,21 @@
-self.addEventListener('install', function(event) {
-	event.waitUntil(
-		caches.open('stuff').then(function(cache) {
-			return cache.addAll([
-				'/',
-				'/index.html',
-				'/restaurant.html',
-				'/img/',
-				'/css/styles.css',
-				
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open('stuff').then(function (cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/restaurant.html',
+        '/img/',
+        '/css/styles.css',
 
-			]);
-		})
-	);
+
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches.match(event.request)
-	);
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+  );
 });
